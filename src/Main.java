f0ba99b98587ae4f589b73afd1067f7fb7ac3503
Arthur Gomes <72204367+arthuzim@users.noreply.github.com>
2023-05-10 18:06:52 -0300

    2

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,25 @@ import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import javax.swing.JOptionPane;
 
 public class Main {
 
-    public static boolean verificaHistorico(ArrayList<Integer> lAnteriores,int futuraPosicao) {
-        System.out.println("Checking historico");
+    public static boolean verificaHistorico(HashSet<Integer> lAnteriores, int futuraPosicao) {
+        System.out.println("Checking Linha");
+//        System.out.println("historico " + lAnteriores);
         return !lAnteriores.contains(futuraPosicao);
     }
+        public static boolean verificaHistoricoAnteriores(HashSet<String> anteriores, int linhaProcurada, int colunaProcurada) {
+            return !anteriores.contains(linhaProcurada + "," + colunaProcurada);
 
-    public static boolean verificaPosicao(ArrayList<Integer> cAnteriores, int linhaOuColuna) {
-        System.out.println("Checking linha ou coluna");
+    }
 
+    public static boolean verificaPosicao(HashSet<Integer> cAnteriores, int linhaOuColuna) {
+        System.out.println("Checking coluna");
+//        System.out.println("Posiçao "+ cAnteriores);
         return !cAnteriores.contains(linhaOuColuna);
     }
 
@@ -53,7 +59,7 @@ public class Main {
                 lines.add(strLine);
 
             fstream.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null,
                     "N�o foi poss�vel ler o arquivo de entrada",
                     "Error",
@@ -74,22 +80,17 @@ public class Main {
         int cSaida = -1; // Sa�da: coluna
 
         // percorre toda a matriz (a partir da segunda linha do arquivo texto) para identificar a posi��o inicial e a sa�da
-        for (int l = 1; l < lines.size(); l++)
-        {
+        for (int l = 1; l < lines.size(); l++) {
             String[] line = lines.get(l).split(" ");
-            for (int c = 0; c < line.length; c++)
-            {
+            for (int c = 0; c < line.length; c++) {
                 String ll = line[c];
                 matriz[l - 1][c] = ll;
 
-                if (ll.equals("X"))
-                {
+                if (ll.equals("X")) {
                     // Posi��o inicial
                     lAtual = l - 1;
                     cAtual = c;
-                }
-                else if (ll.equals("0") && (l == 1 || c == 0 || l == lines.size() - 1 || c == line.length - 1))
-                {
+                } else if (ll.equals("0") && (l == 1 || c == 0 || l == lines.size() - 1 || c == line.length - 1)) {
                     // Sa�da
                     lSaida = l - 1;
                     cSaida = c;
@@ -104,83 +105,99 @@ public class Main {
         // Guarda o trajeto em uma list de string e j� inicia com a posi��o de origem
         List<String> resultado = new ArrayList<String>();
         resultado.add("O [" + (lAtual + 1) + ", " + (cAtual + 1) + "]");
-        var lAnterior  = lAtual;
-        var cAnterior  = cAtual;
-        ArrayList<Integer> lAnteriores = new ArrayList<Integer>();
-        ArrayList<Integer> cAnteriores = new ArrayList<Integer>();
-        Integer[][] posicoesAnteriores = new Integer[lAnterior][cAnterior];
+        var lAnterior = lAtual;
+        var cAnterior = cAtual;
+
+        HashSet<String> anteriores = new HashSet<>();
         // Percorre a matriz (labirinto) at� encontrar a sa�da, usando as regras de prioridade e posi��es n�o visitadas, e vai armazenando o trajeto na list resultado
         boolean achouSaida = lAtual == lSaida && cAtual == cSaida;
-        while (!achouSaida)
-        {
+        while (!achouSaida) {
 
 
+            System.out.println("********");
+            System.out.println("L: "+ lAtual + " C: " + cAtual);
+            System.out.println("********");
+            String historico = String.format("%d,%d", lAtual, cAtual);
+            System.out.println(historico);
+            System.out.println("********");
 
 
 
             //if Pode ir para cima? Ent�o move e guarda o movimento C na list resultado
-            if (lAtual > 0 && matriz[(lAtual-1)][cAtual].equals("0") &&(verificaHistorico(lAnteriores, (lAtual - 1)) || verificaPosicao(cAnteriores, cAtual))){
-                System.out.println("antes"+lAtual);
+            if (lAtual > 0 && matriz[(lAtual - 1)][cAtual].equals("0") && verificaHistoricoAnteriores(anteriores,(lAtual-1),cAtual)) {
+                System.out.println("antes " + lAtual);
+                lAtual = lAtual - 1;
                 lAnterior = lAtual;
                 cAnterior = cAtual;
-                    lAtual =lAtual - 1;
 
-                    resultado.add("C[" + lAtual + "," + cAtual + "]");
-                    System.out.println("e " + lAtual + "," + cAtual + "moveu para cima");
+                resultado.add("C [" + (lAtual + 1) + "," + (cAtual+1) + "]");
+                System.out.println("e " + lAtual + "," + cAtual + "moveu para cima");
 
-                    lAnteriores.add(lAtual);
 
             }
+
             //else if Pode ir para esquerda? Ent�o move e guarda o movimento E na list resultado
-            else if (cAtual > 0 && matriz[lAtual][(cAtual-1)].equals("0") && (verificaHistorico(cAnteriores, (cAtual - 1)) || verificaPosicao(lAnteriores, lAtual))){
-                System.out.println("antes"+cAtual);
+            else if (cAtual > 0 && matriz[lAtual][(cAtual - 1)].equals("0") && verificaHistoricoAnteriores(anteriores,lAtual,(cAtual-1))) {
+                System.out.println("antes" + cAtual);
                 lAnterior = lAtual;
                 cAnterior = cAtual;
-                cAtual -=1;
+                cAtual -= 1;
 
-                resultado.add("E[" + lAtual + "," + cAtual + "]");
+
+                resultado.add("E [" + (lAtual+1) + "," + (cAtual+1) + "]");
                 System.out.println("e " + lAtual + "," + cAtual + "moveu para esquerda");
-                cAnteriores.add(cAtual);
 
 
             }
             //else if Pode ir para direita? Ent�o move e guarda o movimento D na list resultado
-            else if ( matriz[lAtual][(cAtual + 1)].equals("0") && cAtual < extremidadeLinha && (verificaHistorico(cAnteriores, (cAtual + 1)) || verificaPosicao(lAnteriores, lAtual))) {
+            else if (matriz[lAtual][(cAtual + 1)].equals("0") && cAtual < extremidadeColuna  && verificaHistoricoAnteriores(anteriores,lAtual,(cAtual+1))) {
                 System.out.println("Antes " + cAtual);
                 lAnterior = lAtual;
                 cAnterior = cAtual;
-                    cAtual += 1;
-                    resultado.add("D[" + lAtual + "," + cAtual + "]");
-                    System.out.println("e " + lAtual + "," + cAtual + "moveu para direita");
-
-
-                    cAnteriores.add(cAtual);
-
+                cAtual += 1;
+                resultado.add("D [" + (lAtual+1) + "," + (cAtual+1) + "]");
+                System.out.println("e " + lAtual + "," + cAtual + "moveu para direita");
 
 
             }
             //else if Pode ir para baixo?  Ent�o move e guarda o movimento B na list resultado
-            else if ( matriz[(lAtual + 1)][cAtual].equals("0") && lAtual < extremidadeColuna && (verificaHistorico(lAnteriores, (lAtual + 1)) || verificaPosicao(cAnteriores, cAtual))) {
-                System.out.println("antes"+lAtual);
+            else if (matriz[(lAtual + 1)][cAtual].equals("0") && lAtual <  extremidadeLinha && verificaHistoricoAnteriores(anteriores,(lAtual+1),cAtual)) {
+                System.out.println("antes" + lAtual);
 
                 lAnterior = lAtual;
                 cAnterior = cAtual;
-                    lAtual =lAtual +1;
-                    resultado.add("B[" + lAtual + "," + cAtual + "]");
-                    System.out.println("e " + lAtual + "," + cAtual + "moveu para Baixo");
+                lAtual = lAtual + 1;
+                resultado.add("B [" + (lAtual+1) + "," + (cAtual+1) + "]");
+                System.out.println("e " + lAtual + "," + cAtual + "moveu para Baixo");
 
-                    lAnteriores.add(lAtual);
 
 
             }
             //else tem que voltar para a posi��o anterior
             else {
                 System.out.println("voltando posiçao");
-                lAtual = lAnterior;
-                cAtual = cAnterior;
+
+                if(matriz[(lAtual - 1)][cAtual].equals("0") && !verificaHistoricoAnteriores(anteriores,(lAtual-1),cAtual)){
+                    lAtual -=1;
+                    resultado.add("C [" + (lAtual+1) + "," + (cAtual+1) + "]");
+                }
+                else if(matriz[(lAtual + 1)][cAtual].equals("0") && !verificaHistoricoAnteriores(anteriores,(lAtual+1),cAtual)){
+                    lAtual +=1;
+                    resultado.add("B [" + (lAtual+1) + "," + (cAtual+1) + "]");
+                }
+                else if(matriz[lAtual][(cAtual - 1)].equals("0") && !verificaHistoricoAnteriores(anteriores,lAtual,(cAtual-1))){
+                    cAtual -=1;
+                    resultado.add("E [" + (lAtual+1) + "," + (cAtual+1) + "]");
+                }
+                else if(matriz[lAtual][(cAtual + 1)].equals("0") && !verificaHistoricoAnteriores(anteriores,lAtual,(cAtual+1))){
+                    cAtual += 1;
+                    resultado.add("D [" + (lAtual+1) + "," + (cAtual+1) + "]");
+
+                }
+
             }
             // Achou a sa�da?
-
+            anteriores.add(lAtual+ "," + cAtual);
             achouSaida = lAtual == lSaida && cAtual == cSaida;
 
         }
@@ -203,7 +220,7 @@ public class Main {
             }
 
             bw.close();
-        }catch (Exception e) {
+        } catch (Exception e) {
             JOptionPane.showMessageDialog(null,
                     "N�o foi poss�vel escreve o arquivo de sa�da",
                     "Error",
@@ -211,5 +228,5 @@ public class Main {
             return;
         }
     }
-
 }
+
